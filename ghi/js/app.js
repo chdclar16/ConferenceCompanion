7b2fb117f73b0010b6d2ccaf4b057b8e26963e0c
Chad Manuel <chadc_manuel@yahoo.com>
2023-04-04 15:00:42 -0700

    Added the conferences and images

--- ghi/js/app.js
+++ ghi/js/app.js
@@ -1,4 +1,47 @@
-window.addEventListener('DOMContentLoaded', () => {
+window.addEventListener('DOMContentLoaded',async () => {
+    const url = 'http://localhost:8000/api/conferences/'
 
+function createCard(title, description, pictureUrl) {
+    return `
+        <div class="shadow p-3 mb-5 bg-body-tertiary rounded" style="width: 18rem">
+        <img src="${pictureUrl}" class="img-fluid rounded-start" alt="...">
+        <div class="card-body">
+            <h5 class="card-title">${title}</h5>
+            <p class="card-text">${description}</p>
+        <div class="card-footer border-success">${}
+        </div>
+        </div>
+    `;
+    }
 
-});
+    try {
+        const response = await fetch(url);
+
+        if (!response.ok) {
+          // Figure out what to do when the response is bad
+        throw new Error('Response not ok');
+        } else {
+        const data = await response.json();
+
+        for (let conference of data.conferences) {
+            const detailUrl = `http://localhost:8000${conference.href}`;
+            const detailResponse = await fetch(detailUrl);
+            if (detailResponse.ok) {
+                const details = await detailResponse.json();
+                const title = details.conference.name;
+                const description = details.conference.description;
+                const pictureUrl = details.conference.location.picture_url;
+                const html = createCard(title, description, pictureUrl);
+                const column = document.querySelector('.card-columns')
+                column.innerHTML += html;
+                console.log(html)
+            }
+        }
+
+        }
+        } catch (e) {
+            // Figure out what to do if an error is raised
+            console.error('error', error);
+        }
+
+    });
